4bb534492256abda3f919a03b2314fc7cc9e60e0
DANCOOL1337 <kozind311@gmail.com>
2026-03-27 02:48:37 +0500

    Update Task 1. Recursion.pl

--- Task 1. Recursion.pl
+++ Task 1. Recursion.pl
@@ -1,31 +1,21 @@
-
 select_digit(X, [X|T], T).
 
 select_digit(X, [H|T], [H|R]) :- 
     select_digit(X, T, R).
 
-
 unique_num(Num) :-
-    
     select_digit(D1, [1,2,3,4,5,6,7,8,9], Rest1),
-    
     select_digit(D2, [0|Rest1], Rest2),
-    
     select_digit(D3, Rest2, _),
-    
     Num is D1*100 + D2*10 + D3.
-
 
 solve :-
     write('Enter any number and a period (for example, 1.) to start: '),
     read(_),
-    
-    % findall собирает все значения Num, для которых unique_num истинен
+    % findall collects all numbers Num for which unique_num is true
     findall(N, unique_num(N), ResultList),
-    
     write('List of three-digit numbers without repeating digits:'), nl,
     write(ResultList), nl,
     halt.
 
-% Директива для автоматического запуска программы
-:- solve.
+:- initialization(solve).
